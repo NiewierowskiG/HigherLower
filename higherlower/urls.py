d9699031a78ef87e95eee4384c290f51490816b4
GrzegorzNiewierowski <gniewierowski@gmail.com>
2022-04-29 21:01:03 +0200

    lost html and higher/lower buttons

--- higherlower/urls.py
+++ higherlower/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('higherlower/', views.higherlower)
+    path('higherlower/<str:content>', views.higherlower),
+    path('lost/<int:score>', views.lost)
 ]
--- higherlower/views.py
+++ higherlower/views.py
@@ -1,22 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import imdb
 import random
 from .models import Movie
 
-def higherlower(request):
-    queryset = Movie.objects.filter(title__endswith='Dances with Wolves')#last title in Top250
-    if not queryset.exists():
-        print(queryset)
-        ia = imdb.IMDb()
-        Top250Movies = ia.get_top250_movies()
-        for movie in Top250Movies:
-            movietmp = ia.get_movie(movie.movieID)
-            Movie.objects.create(url=f"{movietmp['cover url'].split('_V1_', 1)[0]}.jpg", review=movietmp['rating'], title=movietmp['title'])
-    try:
-        request.movie1 = request.movie2
-        request.movie2 = Movie.objects.filter(id=random.randint(1, 250))
-    except AttributeError:
-        request.movie1 = Movie.objects.filter(id=random.randint(1,250))
-        request.movie2 = Movie.objects.filter(id=random.randint(1, 250))
-    context = {'movie1': request.movie1[0], 'movie2': request.movie2[0]}
+def lost(request, score):
+    return render(request, "higherlower/lost.html", {'score': score})
+
+
+def higherlower(request,content):
+    if content == 'new':
+        queryset = Movie.objects.filter(title__endswith='Dances with Wolves')#last title in Top250
+        if not queryset.exists():
+            print(queryset)
+            ia = imdb.IMDb()
+            Top250Movies = ia.get_top250_movies()
+            for movie in Top250Movies:
+                movietmp = ia.get_movie(movie.movieID)
+                Movie.objects.create(url=f"{movietmp['cover url'].split('_V1_', 1)[0]}.jpg", review=movietmp['rating'], title=movietmp['title'])
+        movie1 = Movie.objects.filter(id=random.randint(1, 250))
+        movie2 = Movie.objects.filter(id=random.randint(1, 250))
+        score = 0
+    else:
+        tmp = content.split(';')
+        score = int(tmp[1])
+        if int(tmp[0]) == 0:
+            return redirect(f'/lost/{score}')
+        score += 1
+        movie1 = Movie.objects.filter(id=tmp[2])
+        movie2 = Movie.objects.filter(id=random.randint(1, 250))
+    if float(movie1[0].review) > float(movie2[0].review):
+        higher = 0
+        lower = 1
+    elif float(movie1[0].review) < float(movie2[0].review):
+        higher = 1
+        lower = 0
+    else:
+        higher = 1
+        lower = 1
+    context = {'movie1': movie1[0], 'movie2': movie2[0], 'score': score, 'higher': higher, 'lower': lower}
     return render(request, "higherlower/higherlower.html", context)
