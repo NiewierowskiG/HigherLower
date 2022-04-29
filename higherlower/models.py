b6fa38828546be73a96ccbd0f32cde55d3e9af0c
GrzegorzNiewierowski <gniewierowski@gmail.com>
2022-04-29 17:42:47 +0200

    Top250 movies to database

--- higherlower/models.py
+++ higherlower/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    url = models.URLField(max_length=200)
+    title = models.CharField(max_length=200)
+    review = models.DecimalField(max_digits=3, decimal_places=1)
--- higherlower/views.py
+++ higherlower/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+import imdb
+import random
+from .models import Movie
 
 def higherlower(request):
+    queryset = Movie.objects.filter(title__endswith='Dances with Wolves')#last title in Top250
+    if not queryset.exists():
+        print(queryset)
+        ia = imdb.IMDb()
+        Top250Movies = ia.get_top250_movies()
+        for movie in Top250Movies:
+            movietmp = ia.get_movie(movie.movieID)
+            Movie.objects.create(url=f"{movietmp['cover url'].split('_V1_', 1)[0]}.jpg", review=movietmp['rating'], title=movietmp['title'])
     context = {}
-    try:
-        print(request.url1)
-    except AttributeError:
-        print("ejoooo")
-    request.url1 = "eoooooooo"
     return render(request, "higherlower/higherlower.html", context)
